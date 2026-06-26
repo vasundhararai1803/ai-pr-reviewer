afb0f67d82787f1d64ca0e0a5f907f6572e06858
Vasundhara Rai <vasundhararai@Vasundharas-MacBook-Air.local>
2026-06-26 06:26:50 +0530

    update test_app.py

--- test_app.py
+++ test_app.py
@@ -5,17 +5,15 @@ from app.main import get_installation_access_token, async_client
 
 @pytest.mark.anyio
 async def test_handshake():
-    # Grab the installation ID from your app settings or previous logs
-    # If you don't know it, look at your GitHub App Settings -> Install App -> The number in the URL!
-    installation_id = 139895444  #  REPLACE THIS with your actual installation ID number
+    installation_id = 139895444  
     
     print("Testing RSA Private Key & JWT Minting Engine...")
     try:
         token = await get_installation_access_token(installation_id)
-        print("🟢 SUCCESS! Cryptographic Handshake Complete.")
+        print("Cryptographic Handshake Complete.")
         print(f"Your temporary, secure Installation Token is: ghs_{token[:10]}...")
     except Exception as e:
-        print("❌ FAILURE! Authentication crashed.")
+        print("Authentication crashed.")
         print(f"Reason: {str(e)}")
         
     await async_client.aclose()
